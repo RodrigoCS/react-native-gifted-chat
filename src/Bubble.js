e76614f540d4b73f74d5e24894a5b9b42c0a34a7
Rodrigo Cervantes Saucedo <rod.cervs@gmail.comgit>
2017-06-13 11:37:44 -0500

    removed ticks to test

--- src/Bubble.js
+++ src/Bubble.js
@@ -1,73 +1,87 @@
-import React from 'react';
+import React from 'react'
 import {
   Text,
   Clipboard,
   StyleSheet,
   TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+  View
+} from 'react-native'
 
-import MessageText from './MessageText';
-import MessageImage from './MessageImage';
-import Time from './Time';
+import MessageText from './MessageText'
+import MessageImage from './MessageImage'
+import Time from './Time'
 
-import { isSameUser, isSameDay, warnDeprecated } from './utils';
+import { isSameUser, isSameDay, warnDeprecated } from './utils'
 
 export default class Bubble extends React.Component {
   constructor(props) {
-    super(props);
-    this.onLongPress = this.onLongPress.bind(this);
+    super(props)
+    this.onLongPress = this.onLongPress.bind(this)
   }
 
   handleBubbleToNext() {
-    if (isSameUser(this.props.currentMessage, this.props.nextMessage) && isSameDay(this.props.currentMessage, this.props.nextMessage)) {
-      return StyleSheet.flatten([styles[this.props.position].containerToNext, this.props.containerToNextStyle[this.props.position]]);
+    if (
+      isSameUser(this.props.currentMessage, this.props.nextMessage) &&
+      isSameDay(this.props.currentMessage, this.props.nextMessage)
+    ) {
+      return StyleSheet.flatten([
+        styles[this.props.position].containerToNext,
+        this.props.containerToNextStyle[this.props.position]
+      ])
     }
-    return null;
+    return null
   }
 
   handleBubbleToPrevious() {
-    if (isSameUser(this.props.currentMessage, this.props.previousMessage) && isSameDay(this.props.currentMessage, this.props.previousMessage)) {
-      return StyleSheet.flatten([styles[this.props.position].containerToPrevious, this.props.containerToPreviousStyle[this.props.position]]);
+    if (
+      isSameUser(this.props.currentMessage, this.props.previousMessage) &&
+      isSameDay(this.props.currentMessage, this.props.previousMessage)
+    ) {
+      return StyleSheet.flatten([
+        styles[this.props.position].containerToPrevious,
+        this.props.containerToPreviousStyle[this.props.position]
+      ])
     }
-    return null;
+    return null
   }
 
   renderMessageText() {
     if (this.props.currentMessage.text) {
-      const {containerStyle, wrapperStyle, ...messageTextProps} = this.props;
+      const { containerStyle, wrapperStyle, ...messageTextProps } = this.props
       if (this.props.renderMessageText) {
-        return this.props.renderMessageText(messageTextProps);
+        return this.props.renderMessageText(messageTextProps)
       }
-      return <MessageText {...messageTextProps}/>;
+      return <MessageText {...messageTextProps} />
     }
-    return null;
+    return null
   }
 
   renderMessageImage() {
     if (this.props.currentMessage.image) {
-      const {containerStyle, wrapperStyle, ...messageImageProps} = this.props;
+      const { containerStyle, wrapperStyle, ...messageImageProps } = this.props
       if (this.props.renderMessageImage) {
-        return this.props.renderMessageImage(messageImageProps);
+        return this.props.renderMessageImage(messageImageProps)
       }
-      return <MessageImage {...messageImageProps}/>;
+      return <MessageImage {...messageImageProps} />
     }
-    return null;
+    return null
   }
 
   renderTicks() {
-    const {currentMessage} = this.props;
+    const { currentMessage } = this.props
     if (this.props.renderTicks) {
-        return this.props.renderTicks(currentMessage);
+      return this.props.renderTicks(currentMessage)
     }
     if (currentMessage.user._id !== this.props.user._id) {
-        return;
+      return
     }
     if (currentMessage.sent || currentMessage.received) {
       return (
         <View style={styles.tickView}>
-          {currentMessage.sent && <Text style={[styles.tick, this.props.tickStyle]}>✓</Text>}
-          {currentMessage.received && <Text style={[styles.tick, this.props.tickStyle]}>✓</Text>}
+          {currentMessage.sent &&
+            <Text style={[styles.tick, this.props.tickStyle]}>✓</Text>}
+          {currentMessage.received &&
+            <Text style={[styles.tick, this.props.tickStyle]}>✓</Text>}
         </View>
       )
     }
@@ -75,69 +89,78 @@ export default class Bubble extends React.Component {
 
   renderTime() {
     if (this.props.currentMessage.createdAt) {
-      const {containerStyle, wrapperStyle, ...timeProps} = this.props;
+      const { containerStyle, wrapperStyle, ...timeProps } = this.props
       if (this.props.renderTime) {
-        return this.props.renderTime(timeProps);
+        return this.props.renderTime(timeProps)
       }
-      return <Time {...timeProps}/>;
+      return <Time {...timeProps} />
     }
-    return null;
+    return null
   }
 
   renderCustomView() {
     if (this.props.renderCustomView) {
-      return this.props.renderCustomView(this.props);
+      return this.props.renderCustomView(this.props)
     }
-    return null;
+    return null
   }
 
   onLongPress() {
     if (this.props.onLongPress) {
-      this.props.onLongPress(this.context, this.props.currentMessage);
+      this.props.onLongPress(this.context, this.props.currentMessage)
     } else {
       if (this.props.currentMessage.text) {
-        const options = [
-          'Copy Text',
-          'Cancel',
-        ];
-        const cancelButtonIndex = options.length - 1;
+        const options = ['Copy Text', 'Cancel']
+        const cancelButtonIndex = options.length - 1
         this.context.actionSheet().showActionSheetWithOptions({
           options,
-          cancelButtonIndex,
-        },
-        (buttonIndex) => {
+          cancelButtonIndex
+        }, buttonIndex => {
           switch (buttonIndex) {
             case 0:
-              Clipboard.setString(this.props.currentMessage.text);
-              break;
+              Clipboard.setString(this.props.currentMessage.text)
+              break
           }
-        });
+        })
       }
     }
   }
 
   render() {
     return (
-      <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
-        <View style={[styles[this.props.position].wrapper, this.props.wrapperStyle[this.props.position], this.handleBubbleToNext(), this.handleBubbleToPrevious()]}>
+      <View
+        style={[
+          styles[this.props.position].container,
+          this.props.containerStyle[this.props.position]
+        ]}>
+        <View
+          style={[
+            styles[this.props.position].wrapper,
+            this.props.wrapperStyle[this.props.position],
+            this.handleBubbleToNext(),
+            this.handleBubbleToPrevious()
+          ]}>
           <TouchableWithoutFeedback
             onLongPress={this.onLongPress}
             accessibilityTraits="text"
-            {...this.props.touchableProps}
-          >
+            {...this.props.touchableProps}>
             <View>
               {this.renderCustomView()}
               {this.renderMessageImage()}
               {this.renderMessageText()}
-              <View style={[styles.bottom, this.props.bottomContainerStyle[this.props.position]]}>
+              <View
+                style={[
+                  styles.bottom,
+                  this.props.bottomContainerStyle[this.props.position]
+                ]}>
                 {this.renderTime()}
-                {this.renderTicks()}
+                {/* {this.renderTicks()} */}
               </View>
             </View>
           </TouchableWithoutFeedback>
         </View>
       </View>
-    );
+    )
   }
 }
 
@@ -145,59 +168,59 @@ const styles = {
   left: StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'flex-start',
+      alignItems: 'flex-start'
     },
     wrapper: {
       borderRadius: 15,
       backgroundColor: '#f0f0f0',
       marginRight: 60,
       minHeight: 20,
-      justifyContent: 'flex-end',
+      justifyContent: 'flex-end'
     },
     containerToNext: {
-      borderBottomLeftRadius: 3,
+      borderBottomLeftRadius: 3
     },
     containerToPrevious: {
-      borderTopLeftRadius: 3,
-    },
+      borderTopLeftRadius: 3
+    }
   }),
   right: StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'flex-end',
+      alignItems: 'flex-end'
     },
     wrapper: {
       borderRadius: 15,
       backgroundColor: '#0084ff',
       marginLeft: 60,
       minHeight: 20,
-      justifyContent: 'flex-end',
+      justifyContent: 'flex-end'
     },
     containerToNext: {
-      borderBottomRightRadius: 3,
+      borderBottomRightRadius: 3
     },
     containerToPrevious: {
-      borderTopRightRadius: 3,
-    },
+      borderTopRightRadius: 3
+    }
   }),
   bottom: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-end'
   },
   tick: {
     fontSize: 10,
     backgroundColor: 'transparent',
-    color: 'white',
+    color: 'white'
   },
   tickView: {
     flexDirection: 'row',
-    marginRight: 10,
+    marginRight: 10
   }
-};
+}
 
 Bubble.contextTypes = {
-  actionSheet: React.PropTypes.func,
-};
+  actionSheet: React.PropTypes.func
+}
 
 Bubble.defaultProps = {
   touchableProps: {},
@@ -210,7 +233,7 @@ Bubble.defaultProps = {
   currentMessage: {
     text: null,
     createdAt: null,
-    image: null,
+    image: null
   },
   nextMessage: {},
   previousMessage: {},
@@ -222,8 +245,8 @@ Bubble.defaultProps = {
   containerToPreviousStyle: {},
   //TODO: remove in next major release
   isSameDay: warnDeprecated(isSameDay),
-  isSameUser: warnDeprecated(isSameUser),
-};
+  isSameUser: warnDeprecated(isSameUser)
+}
 
 Bubble.propTypes = {
   touchableProps: React.PropTypes.object,
@@ -238,26 +261,26 @@ Bubble.propTypes = {
   previousMessage: React.PropTypes.object,
   containerStyle: React.PropTypes.shape({
     left: View.propTypes.style,
-    right: View.propTypes.style,
+    right: View.propTypes.style
   }),
   wrapperStyle: React.PropTypes.shape({
     left: View.propTypes.style,
-    right: View.propTypes.style,
+    right: View.propTypes.style
   }),
   bottomContainerStyle: React.PropTypes.shape({
     left: View.propTypes.style,
-    right: View.propTypes.style,
+    right: View.propTypes.style
   }),
   tickStyle: Text.propTypes.style,
   containerToNextStyle: React.PropTypes.shape({
     left: View.propTypes.style,
-    right: View.propTypes.style,
+    right: View.propTypes.style
   }),
   containerToPreviousStyle: React.PropTypes.shape({
     left: View.propTypes.style,
-    right: View.propTypes.style,
+    right: View.propTypes.style
   }),
   //TODO: remove in next major release
   isSameDay: React.PropTypes.func,
-  isSameUser: React.PropTypes.func,
-};
+  isSameUser: React.PropTypes.func
+}
